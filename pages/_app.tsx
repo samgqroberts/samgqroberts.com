d4dc7f512b8a46180c547bb84ee494b872cbbae1
Sam Roberts <samgqroberts@gmail.com>
2021-09-20 10:56:11 -0400

    add gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
+const GTM_CONTAINER_ID = 'GTM-MNR86CN';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: GTM_CONTAINER_ID });
+  }, []);
   return <Component {...pageProps} />;
 };
 
